c408fe0bf6e502d8f0dc7583394707f26c3bf898
BinhNV <binhnv@exoplatform.com>
2012-07-27 18:13:42 +0700

    commit code get connect to webservice

--- src/com/example/bookstoremb/MainActivity.java
+++ src/com/example/bookstoremb/MainActivity.java
@@ -1,7 +1,22 @@
 package com.example.bookstoremb;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,11 +27,35 @@ public class MainActivity extends ListActivity {
 
     TextView selection;
     String[] items = {"Alice in wonderland", "Jouney to the West", "Shelock Holme", "The Mask"};
-  
+    String URL = "http://192.168.1.130:8080/rest/private/bookstore/searchAllBook";
+    String result;
+    String username = "root";
+    String password = "gtn";
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(URL);
+        request.setHeader("Authorization",
+                      "Basic "
+                          + Base64.encodeToString((username + ":" + password).getBytes(),
+                                                  Base64.NO_WRAP));
+        ResponseHandler<String> handler = new BasicResponseHandler();
+        try {
+          HttpResponse response = client.execute(request);
+          HttpEntity entity = response.getEntity();
+          if (entity != null) {
+            InputStream is = entity.getContent();
+            String responseStr = convertStreamToString(is);
+          }
+        } catch (ClientProtocolException cpe) {
+          cpe.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        client.getConnectionManager().shutdown();
         setListAdapter(new ArrayAdapter<String>(this, R.layout.row, R.id.label, items));
     }
 
@@ -33,5 +72,27 @@ public class MainActivity extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
       super.onListItemClick(l, v, position, id);
     }
+    
+    private String convertStreamToString(InputStream is) {
+
+      BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+      StringBuilder sb = new StringBuilder();
+
+      String line = null;
+      try {
+          while ((line = reader.readLine()) != null) {
+              sb.append(line + "\n");
+          }
+      } catch (IOException e) {
+          e.printStackTrace();
+      } finally {
+          try {
+              is.close();
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+      }
+      return sb.toString();
+  }
     
 }
---
+++ src/com/example/bookstoremb/utils/RestClient.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2003-2012 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.example.bookstoremb.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : BinhNV
+ *          binhnv@exoplatform.com
+ * Jul 27, 2012  
+ */
+public class RestClient {
+  
+  public static enum RequestMethod {
+    GET, POST, PUT, DELETE;
+  }
+  
+  private List<NameValuePair> params;
+  private List<NameValuePair> headers;
+  private String url;
+  private int responseCode;
+  private String errorMessage;
+  private String responseStr;
+  
+  /**
+   * 
+   */
+  public RestClient(String url) {
+    this.url = url;
+    params = new ArrayList<NameValuePair>();
+    headers = new ArrayList<NameValuePair>();
+  }
+
+  public void execute(RequestMethod method) throws Exception {
+    switch (method) {
+      case GET:
+        {
+          String combinedParams = "";
+          if (!params.isEmpty()) {
+            combinedParams += "?";
+            for (NameValuePair p : params) {
+              String paramString = p.getName() + "=" + URLEncoder.encode(p.getValue(), "UTF-8");
+              if (combinedParams.length() > 1) {
+                combinedParams += "&" + paramString;
+              } else {
+                combinedParams += paramString;
+              }
+            }
+          }
+          HttpGet request = new HttpGet(url + combinedParams);
+          for (NameValuePair h : headers) {
+            request.addHeader(h.getName(), h.getValue());
+          }
+          executeRequest(request, url);
+          break;
+        }
+      case POST:
+        {
+          HttpPost request = new HttpPost(url);
+          for (NameValuePair h : headers) {
+            request.addHeader(h.getName(), h.getValue());
+          }
+          if (!params.isEmpty()) {
+            request.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+          }
+          executeRequest(request, url);
+          break;
+        }
+    }
+  }
+  
+  public void addParam(String name, String value) {
+    params.add(new BasicNameValuePair(name, value));
+  }
+  
+  public void addHeader(String name, String value) {
+    headers.add(new BasicNameValuePair(name, value));
+  }
+  
+  /**
+   * @return the responseCode
+   */
+  public int getResponseCode() {
+    return responseCode;
+  }
+
+  /**
+   * @return the errorMessage
+   */
+  public String getErrorMessage() {
+    return errorMessage;
+  }
+
+  /**
+   * @return the response
+   */
+  public String getResponseStr() {
+    return responseStr;
+  }
+  
+  private void executeRequest(HttpUriRequest request, String url) {
+    HttpClient client = new DefaultHttpClient();
+    HttpResponse respone;
+    try {
+      respone = client.execute(request);
+      responseCode = respone.getStatusLine().getStatusCode();
+      errorMessage = respone.getStatusLine().getReasonPhrase();
+      HttpEntity entity = respone.getEntity();
+      if (entity != null) {
+        InputStream is = entity.getContent();
+        responseStr = convertStreamToString(is);
+        is.close();
+      }
+    } catch (ClientProtocolException cpe) {
+      client.getConnectionManager().shutdown();
+      cpe.printStackTrace();
+    } catch (IOException ioe) {
+      client.getConnectionManager().shutdown();
+      ioe.printStackTrace();
+    }
+  }
+  
+  private String convertStreamToString(InputStream is) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    StringBuilder sb = new StringBuilder();
+    String line = "";
+    try {
+      while ((line = reader.readLine()) != null) {
+        sb.append(line + "\n");
+      }
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    } finally {
+      try {
+        is.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return sb.toString();
+  }
+
+}
